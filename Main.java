868c07222de72df0fad84464bac9af452a22674e
Mountblack8185 <135257493+Mountblack8185@users.noreply.github.com>
2023-06-21 22:57:33 +0000

    First attempt of submitting assignment

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Enter a word:");
+  public static void main(String[] args) 
+  {
+    Scanner word = new Scanner(System.in);
+    String WordChosen;
     
-    //Your code will return the length of the entered String
+    System.out.println("Enter a word:");
+    WordChosen = word.nextLine();
+
+    System.out.println("\n" + "The length of your word is: " + WordChosen.length());
+     //Your code will return the length of the entered String
   }
 }
